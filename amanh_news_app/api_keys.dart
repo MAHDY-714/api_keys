9285d42f945878fefb9c67642944cc56d21be8ab
MohmeaD Mahdy <158079560+MAHDY-714@users.noreply.github.com>
2026-03-12 09:47:43 +0200

    Add files via upload

---
+++ amanh_news_app/api_keys.dart
@@ -0,0 +1,22 @@
+abstract class ApiKey {
+  ApiKey._();
+  static List<String> apiKeys = [
+    // hamo elking
+    'pub_14549f0fe2c775bcdce90e652c4a6a741cf6b',
+
+    // mohmead mahdy
+    'pub_7176530eedc45b7639c11c04cbd87174a3fb3',
+
+    // mohamed 719
+    'pub_7176642dc43622a78e1d091f7dd18ad09ed68',
+
+    // momahdy724
+    'pub_45f9e7097a974d9c9760fa8cbec080de',
+
+    // momahdy799
+    'pub_f140d47771f24410ac247e5c63f41447',
+
+    // sayedMohmed1832
+    'pub_be63b3fd68eb433d92e455de8c1c9c16',
+  ];
+}
